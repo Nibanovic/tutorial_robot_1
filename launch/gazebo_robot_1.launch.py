ebbd96760ca300fe1f52c95d10d321e959f60af4
Nibbanovic <120715125+Nibbanovic@users.noreply.github.com>
2023-05-02 13:18:39 +0200

    added a gazebo sim file and made the robot_state_publisher use simulation time

---
+++ launch/gazebo_robot_1.launch.py
@@ -0,0 +1,70 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+    # Declare arguments
+    declared_arguments = []
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "description_package",
+            default_value="tutorial_robot_1",
+            description="Description package of the robot_1. Usually the argument is not set, \
+        it enables use of a custom description.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "prefix",
+            default_value='""',
+            description="Prefix of the joint names, useful for \
+        multi-robot setup. If changed than also joint names in the controllers' configuration \
+        have to be updated.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "use_sim_time",
+            default_value='true',
+            description="set nodes to use sim time for gazebo simulation",
+        )
+    )
+
+    # Initialize Arguments
+    description_package = LaunchConfiguration("description_package")
+    prefix = LaunchConfiguration("prefix")
+    use_sim_time = LaunchConfiguration("use_sim_time")
+
+    # Get URDF via xacro
+    robot_description_content = Command(
+        [
+            PathJoinSubstitution([FindExecutable(name="xacro")]),
+            " ",
+            PathJoinSubstitution(
+                [FindPackageShare(description_package), "urdf", "robot_1.urdf.xacro"]
+            ),
+            " ",
+            "prefix:=",
+            prefix,
+            " ",
+        ]
+    )
+
+    robot_description = {"robot_description": robot_description_content, "use_sim_time": use_sim_time}
+
+    robot_state_publisher_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        output="both",
+        parameters=[robot_description],
+    )
+
+    return LaunchDescription(
+        declared_arguments
+        + [
+            robot_state_publisher_node,
+        ]
+    )
